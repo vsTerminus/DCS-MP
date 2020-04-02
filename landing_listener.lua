b87e15a22080cba933415afe8bf63cdf7f625209
vsTerminus <vsTerminus@vsTerminus.ca>
2020-04-02 15:15:26 -0500

    Mostly fixes
    - Fixed J-11A spawn
    - Increased E-3 Orbit speed
    - Changed E-3 Orbit to racetrack
    - Disabled Easy Comms
    - Moved Kirov farther East so Khasab is safe
    - Added anti-shipping cold starts at Khasab
    - Improvements to landing_listener.lua, should eliminate errors on crash landings
    - Add C-101 spawns
    - Adjust blue carrier group so hopefully they don't crash into each other?

--- landing_listener.lua
+++ landing_listener.lua
@@ -1,20 +1,27 @@
-do
-
-	local function onLanding(event)            
-		if event.id == world.event.S_EVENT_LAND then                    
-			local unit = event.initiator               
-			local coalition = unit:getCoalition() -- 2 == blue
-			local place = unit.place
-			local category = place.category
-			
-			if coalition == 2 and category == 4 and place:getName() == 'Bandar-e-Jask airfield' then
-				trigger.action.setUserFlag(100, 1) -- Victory trigger
-			end	 
-			
-			trigger.action.setUserFlag(200, 1) -- Grunt Birthday Party trigger
-		end    
-	end
-
-	mist.addEventHandler(onLanding)    
-
+local function onLanding(event)            
+	if event.id == world.event.S_EVENT_LAND then                    
+		local unit = event.initiator               
+		local coalition = unit:getCoalition() -- 2 == blue
+		local place = nil
+		local category = nil
+		
+		if unit.place then 
+			place = unit.place 
+			category = place.category
+		end
+		
+		if coalition == 2 and category == 4 and place:getName() == 'Bandar-e-Jask airfield' then
+			trigger.action.setUserFlag(100, 1) -- Victory trigger
+		end	 
+		
+		trigger.action.setUserFlag(200, 1) -- Grunt Birthday Party trigger
+	end    
 end
+
+mist.addEventHandler(onLanding)   
+
+local msg = {}
+msg.text = 'Landing Listener Loaded' 
+msg.displayTime = 1 
+msg.msgFor = {coa = {'all'}} 
+mist.message.add(msg)    
