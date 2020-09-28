30f9397d220b9294d89dcdd41cf21b3ce5b2567f
vsTerminus <vsTerminus@vsTerminus.ca>
2020-09-28 02:51:57 -0500

    Remove unused code

--- 03_markpoint_listener.lua
+++ 03_markpoint_listener.lua
@@ -1,30 +1,5 @@
 markPoint = {}
 
-function print_r(arr, indentLevel)
-    local str = ""
-    local indentStr = "#"
-
-    if(indentLevel == nil) then
-        print(print_r(arr, 0))
-        return
-    end
-
-    for i = 0, indentLevel do
-        indentStr = indentStr.."\t"
-    end
-
-    for index,value in pairs(arr) do
-        if type(value) == "table" then
-            str = str..indentStr..index..": \n"..print_r(value, (indentLevel + 1))
-        else 
-            str = str..indentStr..index..": "..value.."\n"
-        end
-    end
-    return str
-end
-
-
-
 local function onMark(event)    
 	env.info(("onMark Fired"))
 	if ( event.id == world.event.S_EVENT_MARK_ADDED ) then
