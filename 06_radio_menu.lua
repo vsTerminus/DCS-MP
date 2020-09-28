57e31c5c738369096400eb507cd92b658aec3ba3
vsTerminus <vsTerminus@vsTerminus.ca>
2020-09-28 02:52:07 -0500

    Several improvements to spawning
    - Only pop smoke for ground and water units, not air units
    - Tankers now spawn at the mark point and travel 90nm North before returning to Damascus to land
    - Tankers activate TCN and tune their radio immediately now
    - Spawned group description is more reliably displayed now
    - Improved logging considerably with dumper function

--- 06_radio_menu.lua
+++ 06_radio_menu.lua
@@ -4,12 +4,38 @@
 
 local defaultSound = "tsctra00.wav"
 
-local function dumper(var)
+local function dumper(var, indent)
+	
+	if not indent then indent = 0 end
 
-	for k,v in pairs(var) do
-		env.info((k))
-		if type(v) == 'table' then dumper(v) else env.info((k)) end
+	if indent == 0 then env.info(("Dumper Start")) end
+	
+	local pad = ""
+	for i=0,indent do
+		pad = string.format("%s  ", pad)
 	end
+	
+	for k,v in pairs(var) do
+		if ( type(v) == 'table' ) then 
+			env.info((string.format("%s'%s' = {", pad, k)))
+			dumper(v, indent+1) 
+			env.info((string.format("%s}", pad)))
+		elseif type(v) == 'function' then
+			env.info((string.format("%s'%s' = function'", pad, k)))
+		elseif type(v) == 'nil' then
+			env.info((string.format("%s'%s' = nil", k, pad)))
+		elseif type(v) == 'boolean' then
+			local bool = 'false'
+			if v then bool = 'true' end
+			env.info((string.format("%s'%s' = %s", pad, k, bool)))
+		elseif type(v) == 'string' or type(v) == 'number' then
+			env.info((string.format("%s'%s' = '%s'", pad, k, v)))
+		else
+			env.info((string.format("%s'%s' = Unknown Type: %s", pad, k, type(v))))
+		end
+	end
+	
+	if indent == 0 then env.info(("Dumper End")) end
 
 end
 
@@ -17,6 +43,7 @@ local function spawnUnit(args)
 
 	local group = args.group
 	local action = args.action -- Accepted actions are clone, teleport, respawn
+	local cat = args.category -- Accepted categories are land, water, air
 
 	-- If we have a markpoint, we can spawn units there instead of ME coords.
 	if ( markPoint.x ) then
@@ -31,14 +58,17 @@ local function spawnUnit(args)
 		spawnVars.groupName = group.name
 		spawnVars.action = action
 		spawnVars.point = markPoint
-		spawnVars.route = mist.getGroupRoute(group.name, true)
-		dumper(spawnVars.route)
+		if args.route then spawnVars.route = args.route else spawnVars.route = mist.getGroupRoute(group.name, true) end
+		--dumper(spawnVars.route)
 		
 		newGroup = mist.teleportToPoint(spawnVars)
 		
-		-- Drop smoke in the middle of the group
-		avgPoint = mist.getAvgGroupPos(newGroup.name)
-		trigger.action.smoke({x=avgPoint.x, y=avgPoint.y, z=avgPoint.z}, trigger.smokeColor.Red)
+		-- Need to only drop smoke for ground units, or it breaks things.
+		if ( cat ~= 'air' ) then
+			-- Drop smoke in the middle of the group
+			avgPoint = mist.getAvgGroupPos(newGroup.name)
+			trigger.action.smoke({x=avgPoint.x, y=avgPoint.y, z=avgPoint.z}, trigger.smokeColor.Red)
+		end
 	
 	else -- If no mark point defined (yet), just respawn the group at ME coords.
 		mist.respawnGroup(group.name, true)	
@@ -46,12 +76,33 @@ local function spawnUnit(args)
 	
 	-- Tell the user exactly what spawned
 	local msg = {}
-	msg.text = group.description
-	msg.displayTime = 15
+	msg.text = string.format('Spawned: %s', group.description)
+	msg.displayTime = 20
 	msg.msgFor = {coa = {'all'}}
 	if ( group.sound ) then msg.sound = group.sound else msg.sound = defaultSound end
 	mist.message.add(msg)
 	
+end
+
+-- calls spawnUnit after it makes some changes to the group's route.
+local function spawnTanker(args)
+	if markPoint.x then
+		local oldRoute = mist.getGroupRoute(args.group.name, true)
+		local newRoute = oldRoute
+		
+		-- Start at mark point
+		newRoute[1].x = markPoint.x
+		newRoute[1].y = markPoint.z
+		
+		-- Go 90nm North
+		newRoute[2].x = markPoint.x + 166680
+		newRoute[2].y = markPoint.z
+		
+		-- Pass the new route to spawnUnit
+		args.route = newRoute
+	end
+	
+	spawnUnit(args)
 end
 
 -- Note: Re-do this so that it can build the radio menu structure 100% dynamically based on the units table structure.
@@ -71,11 +122,11 @@ local HelicopterMenu	= missionCommands.addSubMenu("Helicopters",SpawnMenu)
 local BoatsMenu			= missionCommands.addSubMenu("Boats",SpawnMenu)
 
 -- Third Level: Groups
-for key in pairs(spawnable.tankers) 	do missionCommands.addCommand(key, TankerMenu, 			function() spawnUnit({group=spawnable.tankers[key], action='respawn'}) end) end
-for key in pairs(spawnable.armor) 		do missionCommands.addCommand(key, ArmorMenu,  			function() spawnUnit({group=spawnable.armor[key], action='clone'}) end) end
-for key in pairs(spawnable.infantry)	do missionCommands.addCommand(key, InfantryMenu,  		function() spawnUnit({group=spawnable.infantry[key], action='clone'}) end) end
-for key in pairs(spawnable.airdefences) do missionCommands.addCommand(key, AirDefenceMenu,  	function() spawnUnit({group=spawnable.airdefences[key], action='clone'}) end) end
-for key in pairs(spawnable.fighters) 	do missionCommands.addCommand(key, ModernFighterMenu,  	function() spawnUnit({group=spawnable.fighters[key], action='clone'}) end) end
-for key in pairs(spawnable.warbirds) 	do missionCommands.addCommand(key, WarBirdMenu,  		function() spawnUnit({group=spawnable.warbirds[key], action='clone'}) end) end
-for key in pairs(spawnable.helicopters)	do missionCommands.addCommand(key, HelicopterMenu,  	function() spawnUnit({group=spawnable.helicopters[key], action='clone'}) end) end
-for key in pairs(spawnable.boats)		do missionCommands.addCommand(key, BoatsMenu,  			function() spawnUnit({group=spawnable.boats[key], action='clone'}) end) end
+for key in pairs(spawnable.tankers) 	do missionCommands.addCommand(key, TankerMenu, 			function() spawnTanker({group=spawnable.tankers[key], category='air', action='respawn'}) end) end
+for key in pairs(spawnable.armor) 		do missionCommands.addCommand(key, ArmorMenu,  			function() spawnUnit({group=spawnable.armor[key], category='land', action='clone'}) end) end
+for key in pairs(spawnable.infantry)	do missionCommands.addCommand(key, InfantryMenu,  		function() spawnUnit({group=spawnable.infantry[key], category='land', action='clone'}) end) end
+for key in pairs(spawnable.airdefences) do missionCommands.addCommand(key, AirDefenceMenu,  	function() spawnUnit({group=spawnable.airdefences[key], category='land', action='clone'}) end) end
+for key in pairs(spawnable.fighters) 	do missionCommands.addCommand(key, ModernFighterMenu,  	function() spawnUnit({group=spawnable.fighters[key], category='air', action='clone'}) end) end
+for key in pairs(spawnable.warbirds) 	do missionCommands.addCommand(key, WarBirdMenu,  		function() spawnUnit({group=spawnable.warbirds[key], category='air', action='clone'}) end) end
+for key in pairs(spawnable.helicopters)	do missionCommands.addCommand(key, HelicopterMenu,  	function() spawnUnit({group=spawnable.helicopters[key], category='air', action='clone'}) end) end
+for key in pairs(spawnable.boats)		do missionCommands.addCommand(key, BoatsMenu,  			function() spawnUnit({group=spawnable.boats[key], category='water', action='clone'}) end) end
